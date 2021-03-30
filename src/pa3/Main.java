821a582e3f25c9936f771fcc01dda39ea2bc3afe
Romano <romano@rasciutto.nl>
2021-03-30 21:36:41 +0200

    Renamed fxml file to fit the example

--- src/pa3/Main.java
+++ src/pa3/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("gui.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("pa3.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 600, 600));
         primaryStage.show();
