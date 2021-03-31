7e58cd9a92b9840e41e151140d41f782cb0c359e
Romano <romano@rasciutto.nl>
2021-03-31 21:18:37 +0200

    Added GUI components for decryption

--- src/pa3/Main.java
+++ src/pa3/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("pa3.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("PA3");
         primaryStage.setScene(new Scene(root, 600, 600));
         primaryStage.show();
     }
